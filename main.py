75b3cbb1ecd701ca7e0541846f03414e6ac26a51
packman700 <dominikwlodarczyk@poczta.onet.pl>
2020-08-20 12:44:23 +0200

    Version v4 I must fix hls system

--- main.py
+++ main.py
@@ -1,68 +1,117 @@
 import tkinter as tk
 from tkinter import *
 import hex
-import pyperclip #This help to copy to clipboard
+import pyperclip  # This help to copy to clipboard
 import rgb_hls
 
 win = Tk()
 
 win.geometry('600x300')
 
-#This update all labels and windows
+  # This update all labels and windows
+
+
+
 def update(a, b, c):
+
     R_value_label.config(text=R_value.get())
     G_value_label.config(text=G_value.get())
     B_value_label.config(text=B_value.get())
 
-    Color1.set(rgb_hls.hue_change(R_value.get(), G_value.get(), B_value.get(), 120))
-    Color5.set(rgb_hls.hue_change(R_value.get(), G_value.get(), B_value.get(), 240))
 
-    update_Color_Window(Color_Window3,hex.combine_hex(R_value.get(),G_value.get(),B_value.get()))
+    # Bright_value.set(
+    #     round(max([R_value.get(), G_value.get(), B_value.get()]) / (255 / 100)))  # Snap to max RGB value
+    # Bright_value_label.config(text=(Bright_value.get()))
+
+    Color1.set(rgb_hls.hue_change(R_value.get(), G_value.get(), B_value.get(), number=1, type=Palette_mode.get()))
+    Color2.set(rgb_hls.hue_change(R_value.get(), G_value.get(), B_value.get(), number=2, type=Palette_mode.get()))
+    Color4.set(rgb_hls.hue_change(R_value.get(), G_value.get(), B_value.get(), number=4, type=Palette_mode.get()))
+    Color5.set(rgb_hls.hue_change(R_value.get(), G_value.get(), B_value.get(), number=5, type=Palette_mode.get()))
+
+    update_Color_Window(Color_Window3,hex.combine_hex(R_value.get(),G_value.get(),B_value.get()))  # center color window
     update_Color_Window(Color_Window1,Color1.get())
+    update_Color_Window(Color_Window2,Color2.get())
+    update_Color_Window(Color_Window4,Color4.get())
     update_Color_Window(Color_Window5,Color5.get())
 
     if can_update_hex_entry: # This allows to enter color
-        Hex_Entry_Value.set(hex.combine_hex(R_value.get(), G_value.get(), B_value.get())) #This update Hex Entry
+        Hex_Entry_Value.set(hex.combine_hex(R_value.get(), G_value.get(), B_value.get()))  # This update Hex Entry
+
+############## Menu Button ###############
+Palette_Menu = Menubutton(text='Pallets',relief=RAISED)
+Palette_Menu.place(relx=0.85, rely=0.85)
+
+Palette_Menu.menu = Menu(Palette_Menu, tearoff=0)
+Palette_Menu['menu'] = Palette_Menu.menu
+
+Palette_mode = StringVar()
+Palette_mode.set('Triad')
+
+Palette_Menu.menu.add_radiobutton(label='Triad',variable=Palette_mode,value='Triad')
+Palette_Menu.menu.add_radiobutton(label='Complementary',variable=Palette_mode,value='Complementary')
+Palette_Menu.menu.add_radiobutton(label='Split-complementary',variable=Palette_mode,value='Split-complementary')
+Palette_Menu.menu.add_radiobutton(label='Monochromatic',variable=Palette_mode,value='Monochromatic')
+Palette_Menu.menu.add_radiobutton(label='Quadrangle',variable=Palette_mode,value='Quadrangle')
+Palette_Menu.menu.add_radiobutton(label='Tetradic',variable=Palette_mode,value='Tetradic')
+Palette_Menu.menu.add_radiobutton(label='Analog',variable=Palette_mode,value='Analog')
+
+
 
 ######### R scale and R labels ###########
 R_value = IntVar()
-R_scale = Scale(win,orient=HORIZONTAL, to=255, length=500, showvalue=False, variable=R_value)
-R_scale.place(relx=0.05, rely=0.7, anchor=W)
+R_scale = Scale(win,orient=HORIZONTAL, to=255, length=450, showvalue=False, variable=R_value)
+R_scale.place(relx=0.05, rely=0.65, anchor=W)
 
 R_symbol = Label(text='R')
-R_symbol.place(relx=0.02, rely=0.7, anchor=W)
+R_symbol.place(relx=0.02, rely=0.65, anchor=W)
 
 R_value_label = Label(text=R_value.get())
-R_value_label.place(relx=0.9, rely=0.7, anchor=W)
+R_value_label.place(relx=0.82, rely=0.65, anchor=W)
 
-R_value.trace('w', update) #iniciate update_R_label when R_value cheange
+R_value.trace('w', update)  # iniciate update_R_label when R_value cheange
 
 ######### G scale and G labels ###########
 G_value = IntVar()
-G_scale = Scale(win,orient=HORIZONTAL, to=255, length=500, showvalue=False, variable=G_value)
-G_scale.place(relx=0.05, rely=0.8, anchor=W)
+G_scale = Scale(win,orient=HORIZONTAL, to=255, length=450, showvalue=False, variable=G_value)
+G_scale.place(relx=0.05, rely=0.75, anchor=W)
 
 G_symbol = Label(text='G')
-G_symbol.place(relx=0.02, rely=0.8, anchor=W)
+G_symbol.place(relx=0.02, rely=0.75, anchor=W)
 
 G_value_label = Label(text=G_value.get())
-G_value_label.place(relx=0.9, rely=0.8, anchor=W)
+G_value_label.place(relx=0.82, rely=0.75, anchor=W)
 
-G_value.trace('w', update) #iniciate update_G_label when G_value cheange
+G_value.trace('w', update)  # iniciate update_G_label when G_value cheange
 
 ######### B scale and B labels ###########
 B_value = IntVar()
-B_scale = Scale(win, orient=HORIZONTAL, to=255, length=500, showvalue=False, variable=B_value)
-B_scale.place(relx=0.05, rely=0.9, anchor=W)
+B_scale = Scale(win, orient=HORIZONTAL, to=255, length=450, showvalue=False, variable=B_value)
+B_scale.place(relx=0.05, rely=0.85, anchor=W)
 
 B_symbol = Label(text='B')
-B_symbol.place(relx=0.02, rely=0.9, anchor=W)
+B_symbol.place(relx=0.02, rely=0.85, anchor=W)
 
 B_value_label = Label(text=B_value.get())
-B_value_label.place(relx=0.9, rely=0.9, anchor=W)
+B_value_label.place(relx=0.82, rely=0.85, anchor=W)
 
-B_value.trace('w', update) #iniciate update_B_label when B_value cheange
+B_value.trace('w', update)  # iniciate update_B_label when B_value cheange
 
+# ########## Light scale and labels##########
+# Bright_value = IntVar()
+# Bright_scale = Scale(win, orient=HORIZONTAL, to=100, length=500, showvalue=False, variable=Bright_value)
+#                      # ,command=lambda *args: update('a','b','c',Bright_move=True))
+# # Bright_scale.bind("<ButtonRelease-1>",lambda *args: update('a','b','c',Bright_move=True))
+#
+# Bright_scale.place(relx=0.05, rely=0.95, anchor=W)
+#
+# Bright_symbol = Label(text=u'\u2600')
+# Bright_symbol.place(relx=0.02, rely=0.95, anchor=W)
+#
+# Bright_value_label = Label(text=Bright_value.get())
+#
+# Bright_value_label.place(relx=0.9, rely=0.95, anchor=W)
+#
+# Bright_value.trace('w', update)  # iniciate update Bright_value_label when Bright_value cheange
 
 ######################################
 ########### Color window #############
@@ -124,9 +173,9 @@ Hex_Entry = Entry(textvariable=Hex_Entry_Value)
 Hex_Entry.place(relx=0.457, rely=0.5, width=50, anchor=CENTER)
 
 def character_limit_enter_color(entry_text):
-    if len(entry_text.get()) > 7: #character limit
+    if len(entry_text.get()) > 7:  # character limit
         entry_text.set(entry_text.get()[:-1])
-    if hex.hex_validation(entry_text.get()) and len(entry_text.get()) > 6: #enter color
+    if hex.hex_validation(entry_text.get()) and len(entry_text.get()) > 6:  # enter color
         global can_update_hex_entry
         can_update_hex_entry = False
         R_value.set(int(entry_text.get()[1:3], 16))
--- rgb_hls.py
+++ rgb_hls.py
@@ -1,16 +1,39 @@
 import colorsys
 import math
 import hex
-# ,number=None, type=None
-def hue_change(r,g,b,hue_spin):
 
+Colors_preset = {'Triad': {1: [120, 30, 0], 2: [120, 0, 0], 4: [240, 0, 0], 5: [240, 30, 0]},
+                 'Complementary': {1: [360, 30, 0], 2: [360, -30, 0], 4: [180, 0, 0], 5: [180, 30, 0]},
+                 'Split-complementary': {1: [150, 30, 0], 2: [150, 0, 0], 4: [210, 0, 0], 5: [210, 30, 0]},
+                 'Monochromatic': {1: [360, 0, -20], 2: [360, 0, -10], 4: [360, 0, 10], 5: [360, 0, 20]}, #
+                 'Quadrangle': {1: [360, 30, 0], 2: [360, -30, 0], 4: [180, 0, 0], 5: [180, 30, 0]}, #
+                 'Tetradic': {1: [90, 0, 0], 2: [0, -30, 0], 4: [180, 0, 0], 5: [270, 0, 0]},
+                 'Analog': {1: [30, 0, 0], 2: [60, 0, 0], 4: [330, 0, 0], 5: [300, 0, 0]}}
+
+
+#
+# POPRAWIC LOGIKEEEEE
+#
+def hue_change(r,g,b,number, type, preset=Colors_preset):
     hls_tuple_color = colorsys.rgb_to_hls(r, g, b)
 
     hls_color = list(hls_tuple_color)
-    hls_color[0] = abs(hls_color[0] + hue_spin/360 - 1) #Poprawić bo coś nie działa :<
+    # H
+    hls_color[0] = abs(hls_color[0] + preset[type][number][0]/360 - 1) #Poprawić bo coś nie działa :<
+    # L
+    if hls_color[1]/255*100-preset[type][number][1] >= 30: # Dać na 30?
+        hls_color[1] = hls_color[1] - preset[type][number][1]*2.55
+    elif preset[type][number][1] < 0:
+        if hls_color[1] + preset[type][number][1]*2.55 > 255:
+            hls_color[1] = 255
+        else:
+            hls_color[1] = hls_color[1] + preset[type][number][1]*2.55
+    else:
+        hls_color[1] = hls_color[1] + preset[type][number][1]*2.55
+
+    #S
+    hls_color[2] = hls_color[2] + preset[type][number][2]*2.55
     rgb_color = colorsys.hls_to_rgb(hls_color[0],hls_color[1],hls_color[2])
 
     return hex.combine_hex(int(rgb_color[0]),int(rgb_color[1]),int(rgb_color[2]))
 
-
-print(hue_change(234,95,250,180))
